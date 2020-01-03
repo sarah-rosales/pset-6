edb22eeeeecedb6a5aa11d4a8e605026331bacd2
Sarah Rosales <srosales@ucvts.org>
2020-01-02 21:55:08 -0500

    named functions

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  document.getElementById("ADD").onclick = addTask;
+  document.getElementById("REMOVE").onclick = removeTask;
+}
+
+const addTask = function() {
+
+}
+const removeTask = function() {
+  
+}
