1fb52d0bc6c96f66594fc63932e7c8672ecfc2e3
Sarah Rosales <srosales@ucvts.org>
2020-01-05 23:07:53 -0500

    the app be partially okay

--- js/app.js
+++ js/app.js
@@ -1,14 +1,51 @@
-window.onload = function() {
-  document.getElementById("ADD").onclick = addTask;
-  document.getElementById("REMOVE").onclick = removeTask;
+const todoItems = [];
+
+var element = document.getElementById("myUL");
+for(var i=0;i<element.length;i++){
+    element[i].style.setProperty("text-decoration", "line-through");
 }
 
-let todoItems = [];
-
-const addTask = function() {
-
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-const removeTask = function() {
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var val = document.createTextNode(inputValue);
+  li.appendChild(val);
+  if (inputValue === '') {
+    alert("I am empty, put words here.");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
